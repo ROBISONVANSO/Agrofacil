f111f5e10332ec315e5c6ed38ab89ac0af364a91
Robison Vanso <robisonvanso@gmail.com>
2016-01-20 20:13:39 -0200

    adicionado comentario comentando o teste

--- src/main/java/br/com/agrofacil/teste/teste.java
+++ src/main/java/br/com/agrofacil/teste/teste.java
@@ -6,14 +6,14 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import br.com.agrofacil.model.Pais;
-import br.com.agrofacil.model.TesteEngenariaReversa;
+import br.com.agrofacil.model.TesteEngenhariaReversa;
 
 public class teste {
 //	private static final String PERSISTENCE_UNIT = "agrofacilPU";
 //
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// comentando o teste
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("agrofacilPU");
 		EntityManager manager = factory.createEntityManager();
 		EntityTransaction trx = manager.getTransaction();
@@ -21,17 +21,20 @@ public class teste {
 		trx.begin();
 		Pais pais = new Pais();
 		pais.setPaisCod("01");
-		pais.setPaisNome("BRASIL 22222");
+		pais.setPaisNome("BRASIL");
+		pais.setPaisIsExc(false);
 		
 		manager.persist(pais);
 		
-		TesteEngenariaReversa t = new TesteEngenariaReversa();
+		TesteEngenhariaReversa t = new TesteEngenhariaReversa();
 	
 		t.setTesteBoolean(true);
 		t.setTesteChar10("aaaaaaaaa");
 		t.setTesteNome("teste 1");
 		
 		manager.persist(t);
+		
+		
 		trx.commit();
 
 	}
